caba58f377c98eda9b2ab39faac079c48071fcee
pedrogilsenarego <pedrogilsenarego@gmail.com>
2022-02-07 01:18:24 +0000

    a

--- src/pages/HomePage/Component0/index.js
+++ src/pages/HomePage/Component0/index.js
@@ -11,6 +11,12 @@ import {
 const Image1 =
 	"https://windows10spotlight.com/wp-content/uploads/2021/10/8c9a96d1134960996924700f5741fe5c.jpg";
 
+const Image2 =
+	"https://www.viajantecomum.com/wp-content/uploads/2016/09/palacio-de-sintra.jpg";
+
+const Image3 =
+	"https://lifecooler.com/files/registos/imagens/381332/331343.jpg";
+
 const Component0 = () => {
 	return (
 		<div style={{ position: "absolute", marginTop: "110vh", width: "100vw" }}>
@@ -29,8 +35,8 @@ const Component0 = () => {
 						<Card sx={{ borderRadius: "0px" }}>
 							<CardMedia component="img" height="140" image={Image1} alt="" />
 							<CardContent>
-								<Typography gutterBottom variant="h5" component="div">
-									Lizard
+								<Typography gutterBottom variant="h5">
+									Azenhas do Mar
 								</Typography>
 								<Typography variant="body2" color="text.secondary">
 									Lizards are a widespread group of squamate reptiles, with over
@@ -41,10 +47,10 @@ const Component0 = () => {
 					</Grid>
 					<Grid item xs={4} style={{ overflow: "hidden" }}>
 						<Card sx={{ borderRadius: "0px" }}>
-							<CardMedia component="img" height="140" image={Image1} alt="" />
+							<CardMedia component="img" height="140" image={Image3} alt="" />
 							<CardContent>
 								<Typography gutterBottom variant="h5" component="div">
-									Lizard
+									Sea Food
 								</Typography>
 								<Typography variant="body2" color="text.secondary">
 									Lizards are a widespread group of squamate reptiles, with over
@@ -55,10 +61,10 @@ const Component0 = () => {
 					</Grid>
 					<Grid item xs={4} style={{ overflow: "hidden" }}>
 						<Card sx={{ borderRadius: "0px" }}>
-							<CardMedia component="img" height="140" image={Image1} alt="" />
+							<CardMedia component="img" height="140" image={Image2} alt="" />
 							<CardContent>
 								<Typography gutterBottom variant="h5" component="div">
-									Lizard
+									Sintra
 								</Typography>
 								<Typography variant="body2" color="text.secondary">
 									Lizards are a widespread group of squamate reptiles, with over
